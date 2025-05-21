59e0641d47f8630a593bb66286255cddb30574e3
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-21 01:15:40 -0400

    Code based on Adding Behavior

--- src/index.js
+++ src/index.js
@@ -6,9 +6,45 @@ const getRandomNumber = (max) => {
 console.log(getRandomNumber(10));
 console.log(getRandomNumber(100));
 
-// const getCurrentTime = () => {
-//   const currentDate = new Date();
-//   return currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
-// };
+const getCurrentTime = () => {
+  const currentDate = new Date();
+  return currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+};
 
-// console.log(`The current time is ${getCurrentTime()}.`);
+console.log(`The current time is ${getCurrentTime()}.`);
+
+// global varialbe is called document, it is an object with method
+const fancyParagraphElement = document.getElementById("fancy-paragraph");
+console.log(fancyParagraphElement);
+console.log(fancyParagraphElement.nodeName);
+
+// same as , gives the first matches = 
+// const fancyParagraphElement = docunt.querySelector("fancy-paragraph");
+
+// this will chanage it text to 'I love pizza!
+fancyParagraphElement.textContent = 'I love pizza!'
+
+console.log(
+  fancyParagraphElement.hasAttribute('id') // true
+);
+
+// add class
+fancyParagraphElement.classList.add('highlight');
+
+// create new elements
+const traitsList = document.querySelector('#traits');
+
+const newAppearanceFact = document.createElement('li');
+
+newAppearanceFact.textContent = 'covered in sand (when on a sandy beach)';
+
+traitsList.appendChild(newAppearanceFact);
+
+const anotherFact = document.createElement('li');
+anotherFact.textContent = 'Often have hard shells';
+traitsList.appendChild(anotherFact);
+
+// const appearanceList = document.getElementById('facts__list');
+
+// appearanceList.appendChild(newAppearanceFact);
+
